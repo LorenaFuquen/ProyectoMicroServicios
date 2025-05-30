4df1a8eca318705b0f38b4d76f28664d3b0a3987
Lorena Fuquen <lfuquencald@uniminuto.edu.co>
2025-05-29 22:28:33 -0500

    Implementacion servidor de configuracion

--- Ordenes/src/main/java/com/Ordenes/OrdenesApplication.java
+++ Ordenes/src/main/java/com/Ordenes/OrdenesApplication.java
@@ -2,8 +2,10 @@ package com.Ordenes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.Ordenes")
 @EnableFeignClients (basePackages = "com.Ordenes.client")
 public class OrdenesApplication {
